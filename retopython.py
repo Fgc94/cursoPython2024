a57017a10b592fc126fafcb81f70f06d2523cad6
Fgc94 <gomezcastrohf@gmail.com>
2024-04-08 21:10:57 -0500

    reto

---
+++ retopython.py
@@ -0,0 +1,35 @@
+
+
+print('Hola Mundo') #1000
+
+#1001
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print(f'X = {X}')
+
+
+#1002
+pi = 3.14159
+radio = float(input())
+area = pi* radio**2
+
+print(f'A={area:.4f}')
+
+
+#1003
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+print(f'SOMA = {SOMA}')
+
+
+#1004
+A = int(input())
+B = int(input())
+
+PROD = A * B
+print(f'PROD = {PROD}')
